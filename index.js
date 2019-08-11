0612128d2cea7a1076265ca2c25d10bcc1bd466b
prateek <1022prteeksharma@gmail.com>
2019-08-11 15:30:41 +0530

    new change

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-hello
+const express = require('express'),
+http = require('http'),
+app = express(),
+server = http.createServer(app),
+io = require('socket.io').listen(server);
+app.get('/', (req, res) => {
+
+res.send('Chat Server is running on port 3000')
+});
+
+
+server.listen(3000,()=>{
+
+console.log('Node app is running on port 3000')
+
+});
